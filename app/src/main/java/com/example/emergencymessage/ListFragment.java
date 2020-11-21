e222c08f0484510c8612b9152f7544d539fbddbc
yamamishunsuke <ys26247ys@gmail.com>
2020-11-21 20:28:31 +0900

    フラグメント理解 できませんでした

--- app/src/main/java/com/example/emergencymessage/ListFragment.java
+++ app/src/main/java/com/example/emergencymessage/ListFragment.java
@@ -1,17 +1,14 @@
 package com.example.emergencymessage;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import java.util.Map;
-import java.util.Objects;
 
 public class ListFragment extends Fragment {
     // スマホ版とpc版で表示のさせ方を分岐させている。
@@ -23,22 +20,22 @@ public class ListFragment extends Fragment {
     // Bundle　　画面の破棄、再生成の役割を果す。
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        AppCompatActivity activity = Objects.requireNonNull(getActivity());
-        if(activity.findViewById(R.id.detailsFrame) != null) {
-            isTwoPane = true;
-        }
+//        Activity activity = Objects.requireNonNull(getActivity());
+//        if(activity.findViewById(R.id.detailsFrame) != null) {
+//            isTwoPane = true;
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AppCompatActivity activity = Objects.requireNonNull(getActivity());
+//        Activity activity = Objects.requireNonNull(getActivity());
         View view = inflater.inflate(R.layout.list_fragment, container, false);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                activity, android.R.layout.simple_list_item_1,
-                ListDataSource.getAllNames());
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+//                activity, android.R.layout.simple_list_item_1,
+                ListDataSource.getAllNames();
         ListView list = view.findViewById(R.id.list);
-        list.setAdapter(adapter);
+//        list.setAdapter(adapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             /*
             // タブレットだけの場合
@@ -61,24 +58,24 @@ public class ListFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent,
                                     View view, int i, long id) {
-                FragmentManager manager = Objects.requireNonNull(getFragmentManager());
+//                FragmentManager manager = Objects.requireNonNull(getFragmentManager());
                 DetailsFragment fragment = new DetailsFragment();
                 //bundleに新しい値を設定、
                 Bundle bundle = new Bundle();
                 // クリックされたスパイスの名前をBundleに代入、nameという名前で登録
                 bundle.putString("name", (String) parent.getItemAtPosition(i));
-                if (isTwoPane) {
-                    fragment.setArguments(bundle);
-                    // フラグメントの操作かいし
-                    manager.beginTransaction()
-                            // フラグメントの置換、確定
-                            .replace(R.id.detailsFrame, fragment)
-                            .commit();
-                } else {
+//                if (isTwoPane) {
+//                    fragment.setArguments(bundle);
+//                    // フラグメントの操作かいし
+//                    manager.beginTransaction()
+//                            // フラグメントの置換、確定
+//                            .replace(R.id.detailsFrame, fragment)
+//                            .commit();
+//                } else {
                     Intent intent = new Intent(getActivity(), DetailsActivity.class);
                     intent.putExtras(bundle);
                     startActivity(intent);
-                }
+//                }
             }
         });
         return view;
